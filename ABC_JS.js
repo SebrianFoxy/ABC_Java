193ac2dc51a6828ddcb14fbbb50b7652f40e2929
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-18 18:30:29 +0300

    Задание 198

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,15 +1,9 @@
-function flattenArray(arr) {
-    let flattened = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (Array.isArray(arr[i])) {
-            flattened = flattened.concat(flattenArray(arr[i]));
-        } else {
-            flattened.push(arr[i]);
-        }
-    }
-return flattened;
-}
+let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
-let nestedArray = [1, [2, 7, 8], [3, 4, [5, [6, 7]]]];
-let flattenedArray = flattenArray(nestedArray);
-console.log(flattenedArray);
+let result = arr.map(function(elem) {
+	return elem.map(function(num) {
+		return num * num;
+	});
+});
+
+console.log(result);
