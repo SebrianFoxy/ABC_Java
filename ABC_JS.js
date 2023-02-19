357a25973852172d9d7b04638c95d36a86568a76
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-19 17:58:09 +0300

    Задание213

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,5 +1,10 @@
-let arr = ['John', 'Smit', 'development', 'programmer', 2000];
+let arr = ['John', 'Smit', 'development', 'programmer'];
 
-let [name, surname, develop, ...c] = arr
+let [name, surname, ...c] = arr;
 
-console.log(c)
+let position;
+if (arr[3] !== undefined) {
+	position = arr[3];
+} else {
+	position = 'trainee';
+}
