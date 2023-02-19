b310ba00a740e4806a3d1e0b8e58b27e8383ce22
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-19 17:01:39 +0300

    Задание210

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,6 +1,4 @@
-function func() {
-	return ['John', 'Smit', 'development', 'programmer', 2000];
-}
+let arr = ['John', 'Smit', 'development', 'programmer', 2000];
 
-let [name, surname, develop, position, salary] = func();
-console.log(name, surname)
+let [, , , , salary] = arr;
+console.log(salary)
