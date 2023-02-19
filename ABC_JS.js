1849bd126acd67c0a9b7955ea591827f7869c502
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-19 15:09:42 +0300

    Задание201

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,9 +1,4 @@
 let arr = [-1, 1, 5, -2, 10, -5, -7, 15, -20, 2];
 
-let result = arr.filter(elem => elem > 0)
+let result = arr.every(elem => elem > 0)
 console.log(result)
-
-let arr1 = [-1, 1, 5, -2, 10, -5, -7, 15, -20, 2];
-
-let result1 = arr1.filter(elem => (elem > 0 && elem < 10))
-console.log(result1)
