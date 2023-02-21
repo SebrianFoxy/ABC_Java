cf67443421db47f13cd2d71454f13bdbb0b4f3c4
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-21 16:51:16 +0300

    Задание238

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,12 +1,4 @@
-let date1 = '2020-11-31';
-let date2 = '2020-12-01';
+let date1 = new Date(2020, 1, 1);
+let date2 = new Date(2019, 1, 1);
 
-if (date1 > date2){
-    console.log(date1 + " " + 'больше' + " " + date2);
-}
-else if (date1 < date2){
-    console.log(date2 + " " + 'больше' + " " + date1);
-}
-else{
-    console.log("Они равны")
-}
+console.log(date1 > date2)
