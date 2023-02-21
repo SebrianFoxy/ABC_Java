5bbc3201b938dcde98064886d08c54b6a0dba305
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-21 14:06:39 +0300

    Задание229

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,2 +1,5 @@
-let date = new Date(2025, 12, 1);
-console.log(date.getTime());
+let now  = new Date();
+let date = new Date(2015, 11, 4, 23, 59, 59);
+
+let diff = now.getTime() - date.getTime();
+console.log(diff / (1000 * 60 * 60));
