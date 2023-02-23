9438cf8e8682cc9179ea6f83fb8cf99e4bd699d0
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-23 20:38:34 +0300

    Задание245

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,8 +1,18 @@
-function func() {
-	console.log('message');
+function func1() {
+	console.log('1');
+}
+
+function func2() {
+	console.log('2');
+}
+
+function func3() {
+	console.log('3');
 }
 
 
-let button1 = document.querySelector('#elem1', '#elem2', '#elem3', '#elem4', '#elem5');
-button1.addEventListener('click', func)
+let button1 = document.querySelector('#elem');
+button1.addEventListener('click', func1)
+button1.addEventListener('click', func2)
+button1.addEventListener('click', func3)
 
