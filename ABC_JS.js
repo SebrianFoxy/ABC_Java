9a6fbf0501cf4436f9da8ceeddf028008d4362cd
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-23 20:53:27 +0300

    Задание246

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,18 +1,14 @@
-function func1() {
-	console.log('1');
-}
-
-function func2() {
-	console.log('2');
-}
-
-function func3() {
-	console.log('3');
-}
-
-
 let button1 = document.querySelector('#elem');
-button1.addEventListener('click', func1)
-button1.addEventListener('click', func2)
-button1.addEventListener('click', func3)
+let button = document.querySelector('#elem1');
+
+button1.addEventListener('mouseover', function() {
+	console.log('1');
+});
+button.addEventListener('mouseout', function() {
+	console.log('2');
+});
+button.addEventListener('dblclick', function() {
+	console.log('22');
+});
+
 
