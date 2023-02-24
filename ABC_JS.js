6d8013e7dd144272ed94de58be5a6dfec4bd0819
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-24 14:37:49 +0300

    Задание254

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,1 +1,7 @@
-console.log( document.querySelector('#elem').value )
+function func() {
+	this.value = '!!!';
+}
+
+let elem = document.querySelector('#elem');
+
+elem.addEventListener('blur', func);
