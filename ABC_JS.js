ce3b4785263702348cf83d5e0c487810956cdb02
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-24 15:59:14 +0300

    Задание257

--- ABC_JS.js
+++ ABC_JS.js
@@ -4,6 +4,7 @@ let but = document.querySelector('#but')
 but.addEventListener('click', function func(){
 	for (let elem of elems) {
 		elem.textContent = elem.textContent + ' text';
+		console.log(elem.textContent)
 	}
 })
 
