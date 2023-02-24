ebe605ef62ee9a455dbb4ac47d4c4c97eb28b895
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-24 14:50:55 +0300

    Задание255

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,7 +1,15 @@
+let button1 = document.querySelector('#button1');
+let button2 = document.querySelector('#button2');
+let button3 = document.querySelector('#button3');
+let but = document.querySelector('#but');
+
+button1.addEventListener('click', func);
+button2.addEventListener('click', func);
+button3.addEventListener('click', func);
+but.addEventListener('click', function func1(){
+	button1.value += '!'
+})
+
 function func() {
-	this.value = '!!!';
+	console.log(this.value);
 }
-
-let elem = document.querySelector('#elem');
-
-elem.addEventListener('blur', func);
