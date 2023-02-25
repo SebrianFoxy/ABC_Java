2370d872bde46c25a967123b1fb04c4fdb2b93cf
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-25 21:44:41 +0300

    Задание278

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,22 +1,10 @@
-let elem = document.querySelector('#elem');
-let button = document.querySelector('#but')
-let button1 = document.querySelector('#but1')
-let changing = document.querySelector('p')
+let radios = document.querySelectorAll('input[type="radio"]');
+let button = document.querySelector('#button');
 
-button.addEventListener('click', function func(){
-	elem.disabled = !elem.disabled
-})
-
-button1.addEventListener('click', function func1(){
-	elem.checked = !elem.checked
-	if (elem.checked == true){
-		changing.textContent = 'Hello'
+button.addEventListener('click', function() {
+	for (let radio of radios) {
+		if (radio.checked) {
+			console.log(radio.value);
+		}
 	}
-	else{
-		changing.textContent = 'Bye'
-	}
-})
-
-
-
-
+});
