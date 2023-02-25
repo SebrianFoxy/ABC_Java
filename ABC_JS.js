576a2392fa86ec752605f1bac765a79996f4a9f3
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-25 20:36:15 +0300

    Задание276

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,8 +1,22 @@
 let elem = document.querySelector('#elem');
 let button = document.querySelector('#but')
+let button1 = document.querySelector('#but1')
+let changing = document.querySelector('p')
 
 button.addEventListener('click', function func(){
 	elem.disabled = !elem.disabled
 })
+
+button1.addEventListener('click', function func1(){
+	elem.checked = !elem.checked
+	if (elem.checked == true){
+		changing.textContent = 'Hello'
+	}
+	else{
+		changing.textContent = 'Bye'
+	}
+})
+
+
 
 
