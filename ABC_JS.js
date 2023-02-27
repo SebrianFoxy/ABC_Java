9c094191db7b5a8b141951fa5183b53c83239c7d
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-27 22:09:42 +0300

    Задание281

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,7 +1,15 @@
-let myInput = document.getElementById("elem");
+let elem = document.querySelector('#elem');
+let elem1 = document.querySelector('#elem1');
 
-myInput.addEventListener("input", function() {
-	if (this.value.length > 5) {
-    	console.log(myInput.value);
+elem.addEventListener('input', function func(){
+	if (elem.value.length === 2){
+		elem1.focus();
 	}
-});
+})
+
+elem1.addEventListener('input', function func1(){
+	if (elem1.value.length === 2){
+		console.log('ggg')
+		elem1.blur();
+	}
+})
