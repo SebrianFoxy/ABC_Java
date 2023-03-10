f4d549b00d0cc4ceadbf36173c677bddedd0afa4
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-03-10 13:51:08 +0300

    Задание298

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,11 +1,20 @@
-let parent = document.querySelector('#parent');
-let button = document.querySelector('button');
-let block  = document.querySelector('#block');
+elem1.addEventListener('click', function() {
+	console.log('зеленый - погружение');
+}, true);
+elem1.addEventListener('click', function() {
+	console.log('зеленый - всплытие');
+}, false);
 
-button.addEventListener('click', function() {
-	block.classList.add('active'); 
-});
+elem2.addEventListener('click', function() {
+	console.log('голубой - погружение');
+}, true);
+elem2.addEventListener('click', function() {
+	console.log('голубой - всплытие');
+}, false);
 
-parent.addEventListener('click', function() {
-	block.classList.remove('active');
-});
+elem3.addEventListener('click', function() {
+	console.log('красный - погружение');
+}, true);
+elem3.addEventListener('click', function() {
+	console.log('красный- всплытие');
+}, false);
