8e71e1972bffd3bef87a5e97e27ffe29fde89900
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-03-10 20:09:25 +0300

    Задание301

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,9 +1,5 @@
-let elem1 = document.querySelector('#elem1');
-elem1.addEventListener('blur', func);
-
-let elem2 = document.querySelector('#elem2');
-elem2.addEventListener('blur', func);
-
 function func() {
-	console.log(this.value); // выведет или 'text1', или 'text2'
+	console.log(this);
 }
+
+func();
