e2d72240d882394bcb38ced57f2036495d54a7bd
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-03-10 21:55:47 +0300

    Задание308

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,3 +1,11 @@
-setInterval(function() {
-	console.log('!');
-}, 3000);
+let elem = document.getElementById('#elem')
+elem = parseInt(this.elem.textContent)
+
+setInterval(function func(){
+	elem = elem - 1;
+	console.log(elem)
+	if (elem == 1){
+	alert(elem);
+}
+}, 30)
+
