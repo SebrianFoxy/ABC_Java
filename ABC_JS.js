899e8df4f5519559b11fa3199e98010eea2c6db5
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-03-10 21:25:39 +0300

    Задание307

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,8 +1,3 @@
-let elem = document.getElementById('elem');
-
-function func(param1, param2) {
-	console.log(this.value + param1 + param2);
-}
-
-let newFunc = func.bind(elem);
-newFunc('1', '2');
+setInterval(function() {
+	console.log('!');
+}, 3000);
