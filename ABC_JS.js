a596c86bd3f0dd1d7e9ec5e4b78e96521064e9fa
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-03-10 13:18:11 +0300

    Задание294

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,11 +1,6 @@
 let elem = document.querySelector('#elem');
 
 elem.addEventListener('click', function(event) {
-	if (event.altKey) {
-		console.log('нажат Alt');
-		elem.style.color = 'red';
-	}
-	else{
-		elem.style.color = 'black';
-	}
+	event.preventDefault();
+	console.log('Вы не можете перейти по этой ссылке!');
 });
