3514b9437a0b0a8a6661e234f2b66d2b1779fcc4
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-03-11 16:40:50 +0300

    Задание327

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,5 +1,13 @@
 let parent = document.querySelector('#parent');
-let elem = parent.querySelector('.elem');
 
-let clone = elem.cloneNode(true);
-parent.appendChild(clone);
+let arr = [1, 2, 3, 4, 5];
+
+for (let elem of arr) {
+	let p = document.createElement('p');
+	p.textContent = elem;
+	p.addEventListener('click', function() {
+		let currentValue = parseInt(this.textContent);
+		this.textContent = currentValue + 1;
+	});
+	parent.appendChild(p);
+}
