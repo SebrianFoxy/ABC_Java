65656d4990be2187dd2ffb11e8fb5f2ddce80248
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-03-11 16:43:14 +0300

    Задание328

--- ABC_JS.js
+++ ABC_JS.js
@@ -1,13 +1,13 @@
-let parent = document.querySelector('#parent');
+let elems = document.querySelector('#elem');
 
 let arr = [1, 2, 3, 4, 5];
 
 for (let elem of arr) {
-	let p = document.createElement('p');
-	p.textContent = elem;
-	p.addEventListener('click', function() {
+	let li = document.createElement('li');
+	li.textContent = elem;
+	li.addEventListener('click', function() {
 		let currentValue = parseInt(this.textContent);
-		this.textContent = currentValue + 1;
+		this.textContent = currentValue + 1 + '!';
 	});
-	parent.appendChild(p);
+	elems.appendChild(li);
 }
